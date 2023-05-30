0e0525f1190309a09747dc4564fbac0f434ce97f
karlangelolim19@gmail.com <karl@hivelabstech.com>
2023-05-30 12:58:09 +0800

    added SmartHelmet code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SmartHelmet
+
+## Steps to Setup SmartHelmet
+1. Clone this repository 
+```
+git clone https://github.com/pd1drone/SmartHelmet
+```
+2. Get the LiquidCrystal_I2C, RTClib, Time, UnixTime folders and copy it to the folder libraries it is located the arduino codes has been save it will have a libraries folder. The default folder of arduino is in /Documents/Arduino/libraries/
+![Libraries-Directory](Libraries-Directory.PNG)
+
+3. Setup the Schematic Diagram Below
+![Smart-Helmet-Circuit](SmartHelmet.png)
+
+4. After setting up the Schematic Diagram Open the SmartHelmet.ino file in arduino and then upload.
+
---
+++ SmartHelmet.ino
@@ -0,0 +1,120 @@
+
+#include <Servo.h>
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Push button
+const int IRSensor = 2;
+const int PushBtnDown = 6;
+const int EnableManualVisor = 9;
+const int VisorBtn = 10;
+
+// Servo motor
+Servo servoAutoFit;
+Servo servoVisor;
+
+bool IsAutomaticVisor = true;
+bool IsVisorDown = true;
+int VisorDown = 0;
+int VisorUp = 135;
+
+int IRSensorPreviousValue = 1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(IRSensor, INPUT);
+  pinMode(PushBtnDown, INPUT_PULLUP);
+  pinMode(EnableManualVisor, INPUT_PULLUP);
+  pinMode(VisorBtn, INPUT_PULLUP);
+  servoVisor.attach(5);
+  servoVisor.write(VisorDown);
+}
+
+void loop() {
+
+  float temp = dht.readTemperature();
+  if (isnan(temp)) {
+    temp = 0.0;
+  }
+  int IRSensorValue = digitalRead(IRSensor);
+  int PushbtnDownValue = digitalRead(PushBtnDown);
+  int enableVisorValue = digitalRead(EnableManualVisor);
+
+  Serial.println(enableVisorValue);
+
+
+  while (enableVisorValue == 0) {
+    int VisorValue = digitalRead(EnableManualVisor);
+    if (VisorValue == 1) {
+      IsAutomaticVisor = !IsAutomaticVisor;
+      break;
+    }
+  }
+
+  Serial.print("Automatic: ");
+  Serial.println(IsAutomaticVisor);
+  if (IsAutomaticVisor) {
+    Serial.print(temp);
+    Serial.print(F("°C "));
+    Serial.println();
+    if (temp >= 31.0) {
+      servoVisor.write(VisorUp);
+      IsVisorDown = false;
+    }
+
+    if (temp <= 28.0) {
+      servoVisor.write(VisorDown);
+      IsVisorDown = true;
+    }
+
+  } else {
+    int VisorManualValue = digitalRead(VisorBtn);
+    while (VisorManualValue == 0) {
+      int VisorManualValue = digitalRead(VisorBtn);
+      if (VisorManualValue == 1) {
+        if (IsVisorDown) {
+          servoVisor.write(VisorUp);
+        } else if (!IsVisorDown) {
+          servoVisor.write(VisorDown);
+        }
+        IsVisorDown = !IsVisorDown;
+        break;
+      }
+    }
+  }
+
+  if (IRSensorValue == 1){
+    IRSensorPreviousValue = 1;
+  }
+
+  if(IRSensorPreviousValue == 1){
+    if (IRSensorValue == 0) {
+      servoAutoFit.attach(3);
+      servoAutoFit.write(180);
+      delay(5000);
+      servoAutoFit.detach();
+      IRSensorPreviousValue = 0;
+    }
+  }
+
+
+
+
+  if (PushbtnDownValue == 0) {
+    while (true) {
+      PushbtnDownValue = digitalRead(PushBtnDown);
+      servoAutoFit.attach(3);
+      servoAutoFit.write(0);
+
+      if (PushbtnDownValue == 1) {
+        servoAutoFit.detach();
+        break;
+      }
+    }
+  }
+}
